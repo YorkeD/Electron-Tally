d2240373d604e06f5287310afd8a9f67a8042881
yorkeD <duyongqiang@ezhaoge.com>
2018-01-24 16:22:05 +0800

    first commit , init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-electronjs'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
